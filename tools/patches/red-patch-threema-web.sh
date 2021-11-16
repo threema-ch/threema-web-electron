cb724cb9a8a453a559f1d9fd6b0b65a3b3f5518a
Jarem Forster <jarem.forster@threema.ch>
2021-11-16 13:31:23 +0100

    Change Threema Web patching to work with the next Threema Web release

--- tools/patches/red-patch-threema-web.sh
+++ tools/patches/red-patch-threema-web.sh
@@ -9,9 +9,21 @@ fi
 
 cd "app/dependencies/threema-web/release/$threema_web_version/"
 
-sed -i.bak -E "s/SALTYRTC_HOST:[^,]*,/SALTYRTC_HOST:\"saltyrtc-staging.threema.ch\",/g" *.bundle.js
-sed -i.bak -E "s/PUSH_URL:\s*\"http[^\"]*\",/PUSH_URL:\"https:\/\/push-web-staging.threema.ch\/push\",/g" *.bundle.js
+export SALTYRTC_HOST=saltyrtc-staging.threema.c
+export PUSH_URL=https://push-web-staging.threema.ch/push
+
+if [[ ! -f userconfig.js ]]; then
+    echo "Error: Userconfig not found"
+    exit 1
+fi
+echo '// Overrides by red-patch-threema-web.sh' >> userconfig.js
+if [ ! -z "${SALTYRTC_HOST:-}" ]; then
+    echo "window.UserConfig.SALTYRTC_HOST = '${SALTYRTC_HOST}';" >> userconfig.js
+fi
+if [ ! -z "${PUSH_URL:-}" ]; then
+    echo "window.UserConfig.PUSH_URL = ${PUSH_URL};" >> userconfig.js
+fi
+
 sed -i.bak -E "s/CONSOLE_LOG_LEVEL:[^,]*,/CONSOLE_LOG_LEVEL:\"debug\",/g" *.bundle.js
-sed -i.bak -E "s/ARP_LOG_TRACE:[^,]*,/ARP_LOG_TRACE:false,/g" *.bundle.js
 
 cd ../../../../..
